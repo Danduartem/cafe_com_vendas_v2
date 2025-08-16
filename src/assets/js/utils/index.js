137dc88f34d00c242e93544eda5d559eb02d1083
Daniel Menezes <daniel.menezes@outlook.com>
2025-08-16 21:33:09 +0100

    chore(cleanup): remove unused Navigation component; disable exports for unused Cloudinary utils; keep design intact

--- src/assets/js/utils/index.js
+++ src/assets/js/utils/index.js
@@ -6,12 +6,4 @@
 export { throttle, debounce } from './throttle.js';
 export { safeQuery, safeQueryAll, calculateSlidesPerView, generateId } from './dom.js';
 export { Animations } from './animations.js';
-export {
-  cloudinaryUrl,
-  responsiveImageSources,
-  generateSrcset,
-  generatePlaceholder,
-  youTubeThumbnail,
-  RESPONSIVE_BREAKPOINTS,
-  IMAGE_PRESETS
-} from './cloudinary.js';
+// Cloudinary helpers are currently unused; export can be restored if needed
